feb862b347fb5d7caaa6d55aae9b2cc6dd42e11b
lthnoname <henryle987654@gmail.com>
2026-09-07 13:21:09 +0700

    Cập nhật User.cs

--- FinancialManagement/Models/User.cs
+++ FinancialManagement/Models/User.cs
@@ -29,5 +29,9 @@ namespace FinancialManagement.Models
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public bool IsActive { get; set; }
 
+        //Navigation Properties
+        public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
+        public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+
     }
 }
