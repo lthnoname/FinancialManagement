091a1e3ad7dff06af6ab71f42835b025e34588f1
lthnoname <henryle987654@gmail.com>
2026-09-07 13:15:47 +0700

    thêm model Transaction và Category

---
+++ FinancialManagement/Models/Category.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Transactions;
+
+namespace FinancialManagement.Models
+{
+    [Table("Categories")]
+    public class Category
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tên danh mục")]
+        [StringLength(100, ErrorMessage = "Tên danh mục không được vượt quá 100 ký tự")]
+        public string CategoryName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 'I' = Income (Khoản thu), 'E' = Expense (Khoản chi)
+        /// </summary>
+        [Required]
+        [StringLength(1)]
+        public string Type { get; set; } = "E";
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        //Navigation property
+        [ForeignKey("UserID")]
+        public virtual User? User { get; set; }
+
+        public  virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
---
+++ FinancialManagement/Models/Transaction.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace FinancialManagement.Models
+{
+    [Table("Transactions")]
+    public class Transaction
+    {
+        [Key] [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int TransactionID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public int CategoryID { get; set; }
+
+        /// <summary>
+        /// 'I' = Income (Khoản thu), 'E' = Expense (Khoản chi)
+        /// </summary>
+        [Required]
+        [StringLength(1)]
+        public string CategoryType { get; set; } = "E";
+
+        [Required(ErrorMessage = "Vui lòng nhập số tiền")]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Số tiền phải lớn hơn 0")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime TransactionDate { get; set; } = DateTime.Now;
+
+        [StringLength(500, ErrorMessage = "Ghi chú không vượt quá 500 từ")]
+        public string Note { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+
+        // Navigation Properties
+        [ForeignKey("UserId")]
+        public virtual User? User { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category? Category { get; set; }
+
+    }
+}
